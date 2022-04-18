285c7953ad2ff6780af5e0dd9dcd43047728b087
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-18 15:42:29 -0500

    Agregando ultimos funcionalidades para cadenas

---
+++ js/Fundamentos/cadenas.js
@@ -0,0 +1,11 @@
+//Cadenas de texto se agrupan "" p ''
+var nombre ="Richard"
+var cadena = "Hola " + nombre;
+var resultados = cadena.indexOf("camila")
+console.log(resultados)
+console.log(cadena.length);
+nombre= nombre.replace("R","J");
+console.log(nombre)
+console.log(nombre.slice(2,3));
+console.log(nombre.toLocaleLowerCase());
+console.log(nombre.toLocaleUpperCase())
