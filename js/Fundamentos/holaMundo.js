17cd6be20102ff4b2a3bb24bd8ea8a1893445076
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-18 15:18:15 -0500

    Agregando primeros temas

---
+++ js/Fundamentos/holaMundo.js
@@ -0,0 +1,4 @@
+console.log("Hola mundo");
+let nombre; 
+nombre ="Richard";
+console.log(nombre)
---
+++ js/Fundamentos/numerosOperaciones.js
@@ -0,0 +1,30 @@
+let edad =19;
+console.log(edad+11);
+edad++;
+console.log(edad);
+++edad;
+console.log(edad);
+let suma = edad + 14.5;
+console.log(suma);
+let resta = edad-5;
+console.log(resta);
+let divicion = edad/2;
+console.log(divicion);
+let multiplicacion = edad*2;
+console.log(multiplicacion);
+let modulo = edad%2;
+console.log(modulo);
+if(modulo==2){
+    console.log("numero para")
+}
+else{
+    console.log("numero impar")
+}
+
+let potencia = Math.pow(edad,2);
+console.log(potencia);
+console.log(Math.round(divicion))
+console.log(Math.ceil(divicion))
+console.log(Math.floor(divicion))
+console.log(Math.PI)
+console.log(Math.sqrt(multiplicacion));
